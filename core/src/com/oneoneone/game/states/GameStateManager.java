f53dcd2264285dea16bc398664a49d52ca028516
David <allirix@hotmail.com>
2016-07-11 15:51:28 +1000

    Comments

--- core/src/com/oneoneone/game/states/GameStateManager.java
+++ core/src/com/oneoneone/game/states/GameStateManager.java
@@ -6,27 +6,30 @@ import java.util.Stack;
 
 /**
  * Created by David on 9/07/2016.
+ * States is an abstract class that is replaced by the inhereted class when called
+ * Currently:
+ *      MenuState ---> GameState TODO --> MenuState --> Next levelState
  */
 public class GameStateManager {
     private Stack<State> states;
 
     public GameStateManager(){
         states = new Stack<State>();
-    }
+    } //declares states
     public void push(State state){
         states.push(state);
     }
     public void pop(){
         states.pop();
     }
-    public void get(State state){
+    public void get(State state){ //pushes argument into active state, i.e. makes PlayState active after MenuState
         states.pop();
         states.push(state);
     }
     public void update(float dt){
-        states.peek().update(dt);
+        states.peek().update(dt); //updates active state
     }
     public void render(SpriteBatch sb){
-        states.peek().render(sb);
+        states.peek().render(sb); //renders active state
     }
 }
