1f2d7b9db53190776c29dcb68f85967528f593fb
David <allirix@hotmail.com>
2016-07-11 15:40:40 +1000

    Perfect Edge connection

--- core/src/com/oneoneone/game/states/PlayState.java
+++ core/src/com/oneoneone/game/states/PlayState.java
@@ -17,58 +17,64 @@ public class PlayState extends State{
     public static final int SCREEN_WIDTH = Gdx.graphics.getWidth();
     public static final int SCREEN_HEIGHT = Gdx.graphics.getHeight();
     public static final float SCALEX = (float)BubbleMath.WIDTH/SCREEN_WIDTH;
-    public static final float SCALEY = (float)BubbleMath.HEIGHT/SCREEN_HEIGHT; //(float) used so int division isnt used
-    private static final int MAX_BUBBLES = 10;
-    private Bubble bubble;
+    public static final float SCALEY = (float)BubbleMath.HEIGHT/SCREEN_HEIGHT;
     private Texture bg;
     private Texture redSpawner;
     private Texture blueSpawner;
-    private float dtsum = 0;
-    private Array<Bubble> bubbles;
-    private Vector2 touch_location;
+    private float dtsum = 0; //collects amount of time that has passed in game
+    private Array<Bubble> bubbles; //Array Container of all bubbles
 
+    /* PlayState(GameStateManager gsm) is called after Menu State
+     * Allocates memory and calls constructors for all data members.
+     */
     public PlayState(GameStateManager gsm) {
-        super(gsm);
-        bubble = new Bubble();
+        super(gsm); //super = active state class
         redSpawner = new Texture("red_spawner.png");
         blueSpawner = new Texture("blue_spawner.png");
         bg = new Texture("play_background.png");
         bubbles = new Array<Bubble>();
-        bubbles.add(new Bubble());
+        bubbles.add(new Bubble()); //creates first bubble
     }
 
+    /* handleInput() checks if the person has touched the screen
+    *
+    */
     @Override
     protected void handleInput() {
         for(int i = 0; i<2; i++) {
-            if (Gdx.input.isTouched(i)) {
+            if (Gdx.input.isTouched(i)) {//multitouch
                 for (Bubble bub : bubbles) {
                     bub.grab_bubble(i);
                 }
             }
         }
     }
-
+    /* update(float dt) is called in GameStateManager (see states.peek().update(dt))
+    * updates the mathematics of everything that happens; velocity, coordinates, inputs,
+    * checking,etc
+    */
     @Override
     public void update(float dt) {
-        handleInput();
-        dtsum = dtsum + dt;
-        if (dtsum > 2) {
-            bubbles.add(new Bubble());
-            dtsum = 0;
+        handleInput(); //calls first to see if screen has been touched before updating
+        dtsum = dtsum + dt; //sums poll time
+        if (dtsum > 2) { //if a certain number of poll times have passed spawn a bubble
+            bubbles.add(new Bubble()); //spawns bubble
+            dtsum = 0; //resets sum poll time
         }
-        for (Bubble bub: bubbles){
+        for (Bubble bub: bubbles){//attempt at brute force collision detection TODO Collision Detection
             for (int i=0;i>bubbles.size; i++) {
-                bub.collides(bubbles.get(i).getBound());
+                bub.collides(bubbles.get(i).getBound());//inserts circle bound of all bubbles into current bubble
             }
-            bub.update(dt);
+            bub.update(dt); //calculates position changes to bubble
         }
     }
-
+    /* render(float dt) draws all sprites to SpriteBatch declared in BubbleMath
+    * draws after all positions and conditions have been calculated in update in BubbleMath render
+    */
     @Override
     public void render(SpriteBatch sb) {
         sb.begin();
         sb.draw(bg,0,0);
-
         for (Bubble bub: bubbles){
             sb.draw(bub.getBubble(),bub.getPosition().x,bub.getPosition().y);
         }
@@ -78,7 +84,7 @@ public class PlayState extends State{
     }
 
     @Override
-    public void dispose() {
+    public void dispose() {//TODO write a dispose method to avoid memory leaks
 
     }
 }
