b87b1a75574a09d741563dc9bdfc7e098f0f2f7f
Rod <MightyRhinox@gmail.com>
2016-07-18 22:49:53 +1000

    first stage particle effects

--- core/src/com/oneoneone/game/sprites/FieldEmitters.java
+++ core/src/com/oneoneone/game/sprites/FieldEmitters.java
@@ -1,7 +1,11 @@
 package com.oneoneone.game.sprites;
 
+import com.badlogic.gdx.graphics.g2d.ParticleEffect;
+
 /**
  * Created by Rod on 18/07/2016.
  */
 public class FieldEmitters {
+    private float position;
+    private ParticleEffect peg;
 }
